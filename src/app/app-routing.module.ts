ce62d5942e17dc58b5b20788fe6863de1cbedb45
Anjireddy.kata <anjireddy.kata@gmail.com>
2019-10-03 09:10:12 +0530

    added guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,7 +13,7 @@ const routes: Routes = [
   {
     path: '',
     component: LayoutComponent,
-    //canActivate: [AuthGuard],
+    canActivate: [NoAuthGuard],
     children: [
       {
         path: 'dashboard',
